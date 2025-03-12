2c53cff622998ad0bfbd5b35630d77b4945fb09a
Rakesh <119929341+Rakeshpalla27@users.noreply.github.com>
2025-03-12 23:03:19 +0530

    Add files via upload

---
+++ REACT-router/components/Appnav.jsx
@@ -0,0 +1,6 @@
+import style from "./Appnav.module.css";
+function Appnav() {
+  return <nav className={style.nav}>app navigation</nav>;
+}
+
+export default Appnav;
---
+++ REACT-router/pages/Applayout.jsx
@@ -0,0 +1,12 @@
+import Appnav from "../components/Appnav";
+
+function Applayout() {
+  return (
+    <div>
+      <Appnav />
+      <p>app</p>
+    </div>
+  );
+}
+
+export default Applayout;
